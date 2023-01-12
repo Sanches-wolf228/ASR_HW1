b52a926da0a7cb8a1e44bc63fcd3676e983935da
Sanches-wolf228 <71072356+Sanches-wolf228@users.noreply.github.com>
2023-01-12 12:03:05 +0300

    Update utils.py

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,12 @@
 # Don't forget to support cases when target_text == ''
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
-
+    splitted_target = target_text.split(' ')
+    if len(splitted_target) == 0:
+        return 1
+    return editdistance.distance(splitted_target, pred_text.split(' ')) / len(splitted_target)
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if len(target_text) == 0:
+        return 1
+    return editdistance.distance(target_text, pred_text) / len(target_text)
