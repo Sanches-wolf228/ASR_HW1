d1f00d914b75d8057123eee4e7adaa8f8ff04baa
Sanches-wolf228 <71072356+Sanches-wolf228@users.noreply.github.com>
2023-01-12 12:02:11 +0300

    Update collate.py

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -8,7 +8,31 @@ def collate_fn(dataset_items: List[dict]):
     """
     Collate and pad fields in dataset items
     """
-
-    result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+    spec_lengths = []
+    text_lengths = []
+    simple_texts = []
+    encoded_texts = []
+    
+    for item in dataset_items:
+        spec_lengths.append(item["spectrogram"].shape[-1])
+        text_lengths.append(item["text_encoded"].shape[-1])
+        simple_texts.append(item["text"])
+        encoded_texts.append(item["text_encoded"])
+       
+    feature_length_dim = item["spectrogram"].shape[1]
+    
+    batch_spectrograms = torch.zeros(len(dataset_items), feature_length_dim, max(spec_lengths))
+    batch_encoded_texts = torch.zeros(len(dataset_items), max(text_lengths))
+    for i, item in enumerate(dataset_items):
+        batch_encoded_texts[i, :text_lengths[i]] = item["text_encoded"]
+        batch_spectrograms[i, :, :spec_lengths[i]] = item["spectrogram"]
+    
+    text_lengths = torch.tensor(text_lengths).int()
+    batch_encoded_texts = torch.tensor(batch_encoded_texts).int()
+    
+    return {
+        "spectrogram": batch_spectrograms,
+        "text_encoded": batch_encoded_texts,
+        "text_encoded_length": text_lengths,
+        "text": simple_texts
+    }
