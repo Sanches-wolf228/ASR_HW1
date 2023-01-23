5faadff13068891d1fae65a5a2abba49d75d8feb
Sanches-wolf228 <71072356+Sanches-wolf228@users.noreply.github.com>
2023-01-24 00:46:35 +0300

    implement beam-search

--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -1,14 +1,13 @@
 from typing import List, NamedTuple
+from collections import defaultdict
 
 import torch
 
 from .char_text_encoder import CharTextEncoder
 
-
 class Hypothesis(NamedTuple):
     text: str
     prob: float
-
 
 class CTCCharTextEncoder(CharTextEncoder):
     EMPTY_TOK = "^"
@@ -30,6 +29,19 @@ class CTCCharTextEncoder(CharTextEncoder):
                 res.append(inds[i])
         return ''.join([self.ind2char[x] for x in res])
 
+    def __extend_and_merge__(self, dp, prob):
+        new_dp = defaultdict(float)
+        for (res, last_char), v in dp.items():
+            for i in range(len(prob)):
+                if self.ind2char[i] == last_char:
+                    new_dp[(res, last_char)] += v * prob[i]
+                else:
+                    new_dp[((res + last_char).replace(self.EMPTY_TOK, ''), self.ind2char[i])] += v * prob[i]
+        return new_dp
+
+    def __cut_beams__(self, dp, beam_size):
+        return dict(list(sorted(dp.items(), key = lambda x: x[1]))[-beam_size:])
+
     def ctc_beam_search(self, probs: torch.tensor, probs_length,
                         beam_size: int = 100) -> List[Hypothesis]:
         """
@@ -37,8 +49,18 @@ class CTCCharTextEncoder(CharTextEncoder):
         """
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
+        if probs_length == None:
+            probs_length = char_length
         assert voc_size == len(self.ind2char)
-        hypos: List[Hypothesis] = []
-        # TODO: your code here
-        raise NotImplementedError
-        return sorted(hypos, key=lambda x: x.prob, reverse=True)
+        dp = {
+            ("", self.EMPTY_TOK) : 1.0
+        }
+        for i in range(min(char_length, probs_length)):
+            prob = probs[i]
+            dp = self.__extend_and_merge__(dp, prob)
+            dp = self.__cut_beams__(dp, beam_size)
+
+        dp = list(sorted([((res + last_char).strip().replace(self.EMPTY_TOK, ''), proba) for (res, last_char), proba in dp.items()],
+                         key = lambda x: -x[1]))
+        
+        return [Hypothesis(res, proba) for res, proba in dp]
