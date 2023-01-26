5f7a375ae2167338cfbdc3386e2ef1c08c274b10
Sanches-wolf228 <71072356+Sanches-wolf228@users.noreply.github.com>
2023-01-27 01:52:15 +0300

    Update __init__.py

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,11 @@
+from hw_asr.augmentations.wave_augmentations.AddColoredNoise import AddColoredNoise
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
+from hw_asr.augmentations.wave_augmentations.PitchShift import PitchShift
+from hw_asr.augmentations.wave_augmentations.TimeStretch import TimeStretch
 
 __all__ = [
-    "Gain"
+    "AddColoredNoise",
+    "Gain",
+    "PitchShift",
+    "TimeStretch"
 ]
