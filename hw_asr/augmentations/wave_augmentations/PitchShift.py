4bcdab8c7535543854730f26c650b88b7d208cb7
Sanches-wolf228 <71072356+Sanches-wolf228@users.noreply.github.com>
2023-01-28 12:59:32 +0300

    Update PitchShift.py

--- hw_asr/augmentations/wave_augmentations/PitchShift.py
+++ hw_asr/augmentations/wave_augmentations/PitchShift.py
@@ -1,11 +1,11 @@
-import torchaudio
+import torch_audiomentations
 from torch import Tensor
 
 from hw_asr.augmentations.base import AugmentationBase
 
 class PitchShift(AugmentationBase):
-    def __init__(self, sample_rate, n_steps, *args, **kwargs):
-        self._aug = torchaudio.transforms.PitchShift(sample_rate, n_steps, *args, **kwargs)
+    def __init__(self, sample_rate, *args, **kwargs):
+        self._aug = torch_audiomentations.PitchShift(sample_rate=sample_rate, *args, **kwargs)
 
     def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
